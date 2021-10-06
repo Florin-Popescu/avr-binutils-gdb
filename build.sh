7cf837a17361edb2392da49dbe72fd1aa024bc21
Florin Popescu <florinsgpopescu@gmail.com>
2021-10-07 02:21:58 +0300

    remove host argument

--- build.sh
+++ build.sh
@@ -2,16 +2,13 @@
 
 case $(uname -o | cut -d '/' -f2) in
 	"Msys")
-		export CFLAGS="-D__USE_MINGW_ACCESS" \
-		export CXXFLAGS="-D__USE_MINGW_ACCESS" \
-		export LDFLAGS='-static' \
-		CONFIG_HOST=--host="x86_64-w64-mingw32"
+		export CFLAGS="-D__USE_MINGW_ACCESS"
+		export CXXFLAGS="-D__USE_MINGW_ACCESS"
+		export LDFLAGS='-static'
 
 		SUDO=
 		;;
 	"Linux")
-		CONFIG_HOST=
-
 		NUMBER_OF_PROCESSORS=$(nproc)
 		SUDO=sudo
 		;;
@@ -21,8 +18,7 @@ mkdir build
 cd build
 
 ../configure \
---target=avr \
-$CONFIG_HOST
+--target=avr
 
 make all -j$NUMBER_OF_PROCESSORS
 $SUDO make install
